3b74fb75a902da3dc563991578890dc5ff59cd0b
Zhaohan Guo <zguo@gs13110.sp.cs.cmu.edu>
2016-03-28 15:40:29 -0400

    testing out pickling for parameters

--- rl.py
+++ rl.py
@@ -1,20 +1,31 @@
 import random
 from util import *
+import cPickle
+
+params = '(dp1\n.'
 
 class Bot(object):
-	def __init__(self):
-		pass
-	
-	def first_hand(self):
-		return random.choice(to_str)
-	
-	def next_hand(self, input):
-		return self.seed
+    def __init__(self):
+        if params is None:
+            self.params = {}
+        else:
+            self.params = cPickle.loads(params)
+
+    def first_hand(self):
+        return random.choice(to_str)
+
+    def next_hand(self, input):
+        return self.seed
 
 # when using exec, use legacy behavior
 if __name__ == '__main__':
-	if not input:
-		bot = Bot()
-		output = bot.first_hand()
-	else:
-		output = bot.next_hand(input)
+    #if not input:
+	#    bot = Bot()
+	#    output = bot.first_hand()
+    #else:
+	#    output = bot.next_hand(input)
+    
+    # try out other stuff
+    bot = Bot()
+    print(repr(bot.params))
+    print(repr(cPickle.dumps(bot.params)))
